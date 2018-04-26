eed12fe323d9ff24cbf3f4e8d92e81df2ae05be2
Nathan <butchernate@gmail.com>
2018-04-26 14:26:09 -0700

    Made the potential curve.

--- Escape_Velocity_Earth/u_curve.py
+++ Escape_Velocity_Earth/u_curve.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib
+matplotlib.use("agg")
 import matplotlib.pyplot as plt
 
 r_earth = 6.371e6 # in meters
@@ -11,6 +13,6 @@ U = -G * m_earth / (radii * r_earth)
 plt.plot(radii,U)
 plt.xlabel("Distance from center of Earth (Earth Radii)")
 plt.ylabel("Potential energy of 1 kg object (J)")
-plt.title("Potential energy curve for a 1kg object above the Earth's surface")
+plt.title("Potential Energy Curve")
 plt.xlim(0,10)
-plt.show()
+plt.savefig("Potential_Curve.png")
